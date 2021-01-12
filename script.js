58e864d8a0b3f5bb8e3323faad60e147fdd0c96b
safirasafitri <63382589+safirasafitri@users.noreply.github.com>
2021-01-12 20:12:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ document.querySelector(".tombol").addEventListener('click', function () {
           }).then((result) => {
             /* Read more about isConfirmed, isDenied below */
             if (result.isConfirmed) {
-              Swal.fire(`${sender} juga sayang banget sama ${nama}`).then(function () {
+              Swal.fire(`${sender} juga sayang banget sama ${nama} !!! huhuhuhuh`).then(function () {
                 Swal.fire({
                   title: 'Seberapa sayang emangnya?',
                   icon: 'question',
