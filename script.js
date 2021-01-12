d7c090529c8ac3fb325c2ddc450e8ce340d20a60
safirasafitri <63382589+safirasafitri@users.noreply.github.com>
2021-01-12 20:14:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ document.querySelector(".tombol").addEventListener('click', function () {
                     }).then((result) => {
                       /* Read more about isConfirmed, isDenied below */
                       if (result.isConfirmed) {
-                        Swal.fire(`Huhu iya ${sender} juga kangen ${nama} , makasihhh yaa`).then(function () {
+                        Swal.fire(`Huhu iya ${sender} juga kangen ${nama}, makasihhh yaa`).then(function () {
                           Swal.fire('Terakhir deh sayang').then(function () {
                             Swal.fire('Coba klik ikon hati di paling bawah dong')
                           })
